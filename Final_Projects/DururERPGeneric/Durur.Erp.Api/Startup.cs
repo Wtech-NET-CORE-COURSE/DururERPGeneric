1050008563e289cfdbde33fd39a8210bd5020106
Ümit Durur <umit_durur@hotmail.com>
2021-12-09 09:09:09 +0300

    Added authentication and startup page

--- Final_Projects/DururERPGeneric/Durur.Erp.Api/Startup.cs
+++ Final_Projects/DururERPGeneric/Durur.Erp.Api/Startup.cs
@@ -3,6 +3,7 @@ using Durur.Modules.Generic.Business.Concrete;
 using Durur.Modules.Generic.DataAccess;
 using Durur.Modules.Generic.DataAccess.Repositories;
 using Durur.Modules.Generic.Entities.Repositories;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -10,9 +11,11 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.IdentityModel.Tokens;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
 
 namespace Durur.Erp.Api
@@ -51,8 +54,23 @@ namespace Durur.Erp.Api
             services.AddScoped<ICountryServices, CountryServices>();
             services.AddScoped<ILocationServices, LocationServices>();
 
+            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(opt=>opt.TokenValidationParameters=new TokenValidationParameters { 
+                ValidateAudience=true,          //Token deðerini kimlerin-hangi uygulamalarýn kullanacaðýný belirler
+                ValidateIssuer=true,            //Oluþturulan token deðerini kim daðýtmýþtýr
+                ValidateLifetime=true,          //Oluþturulan token deðerinin yaþam süresi
+                ValidateIssuerSigningKey=true,  //Üretilen token deðerinin uygulamamýza ait olup olmadýðý ile alakalý security key'i
+                ValidIssuer=Configuration["Token:Issuer"],
+                ValidAudience=Configuration["Token:Audience"],
+                IssuerSigningKey=new SymmetricSecurityKey(Encoding.UTF8.GetBytes(Configuration["Token:SecurityKey"])),
+                ClockSkew=TimeSpan.Zero         //Token süresinin uzatýlmasýný saðlar.
+            });
+
+
             services.AddControllers();
             services.AddRazorPages();
+
+
+            services.AddMvc().AddRazorPagesOptions(opt => opt.Conventions.AddPageRoute("/Login", ""));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
