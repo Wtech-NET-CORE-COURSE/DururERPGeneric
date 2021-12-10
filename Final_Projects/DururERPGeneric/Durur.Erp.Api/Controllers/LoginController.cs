4fbd1500a32af552d38f92815ec7e4b9d4f90d9e
Ümit Durur <umit_durur@hotmail.com>
2021-12-10 19:17:01 +0300

    Added helper class and login controller with authentication

---
+++ Final_Projects/DururERPGeneric/Durur.Erp.Api/Controllers/LoginController.cs
@@ -0,0 +1,66 @@
+﻿using Durur.Erp.Api.TokenModels;
+using Durur.Modules.Generic.DataAccess;
+using Durur.Modules.Generic.Entities.Model;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Durur.Erp.Api.Helpers;
+
+namespace Durur.Erp.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LoginController : ControllerBase
+    {
+        private readonly ErpGenericDbContext context;
+        private readonly IConfiguration configuration;
+        private readonly GenericHelper genericHelper;
+
+        public LoginController(ErpGenericDbContext context, IConfiguration configuration,GenericHelper genericHelper)
+        {
+            this.context = context;
+            this.configuration = configuration;
+            this.genericHelper = genericHelper;
+        }
+
+        [HttpPost("[action]")]
+        public async Task<bool> Create([FromBody] User user)
+        {
+            context.Users.Add(user);
+            await context.SaveChangesAsync();
+            return true;
+        }
+
+
+
+        [HttpPost("[action]")]
+        public async Task<Token> Login([FromBody] UserLogin userLogin)
+        {
+            User user = await context.Users.FirstOrDefaultAsync(w => w.Mail == userLogin.Mail && w.Password == userLogin.Password);
+            if (user != null)
+            {
+                return await genericHelper.CreateToken(user, context, configuration);
+            }
+            return null;
+        }
+
+
+        [HttpPost("[action]")]
+        public async Task<Token> RefreshTokenLogin([FromForm] string refreshToken)
+        {
+            User user = await context.Users.FirstOrDefaultAsync(w => w.RefreshToken == refreshToken);
+            if (user != null && user.RefreshTokenEndTime > DateTime.UtcNow)
+            {
+                return await genericHelper.CreateToken(user, context, configuration);
+            }
+            return null;
+        }
+
+
+    }
+}
---
+++ Final_Projects/DururERPGeneric/Durur.Erp.Api/Helpers/GenericHelper.cs
@@ -0,0 +1,30 @@
+﻿using Durur.Erp.Api.TokenModels;
+using Durur.Modules.Generic.DataAccess;
+using Durur.Modules.Generic.Entities.Model;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Durur.Erp.Api.Helpers
+{
+    public class GenericHelper
+    {
+        public GenericHelper()
+        {
+
+        }
+        public async Task<Token> CreateToken(User user,ErpGenericDbContext context,IConfiguration configuration)
+        {
+            //User için token üretiliyor
+            TokenHandler tokenHandler = new TokenHandler(configuration);
+            Token token = tokenHandler.CreateAccessToken();
+            //Refresh token kullanıcı tablosuna işleniyor
+            user.RefreshToken = token.RefreshToken;
+            user.RefreshTokenEndTime = token.Expiration.AddMinutes(5);
+            await context.SaveChangesAsync();
+            return token;
+        }
+    }
+}
