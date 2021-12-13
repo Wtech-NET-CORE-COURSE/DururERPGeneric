9d7e831d3251eaeb44ddac2a3eacf368020c60c0
Ümit Durur <umit_durur@hotmail.com>
2021-12-13 07:19:06 +0300

    Header Modification - Experimental

--- Final_Projects/DururERPGeneric/Durur.Erp.Api/Pages/Shared/Header.cshtml.cs
+++ Final_Projects/DururERPGeneric/Durur.Erp.Api/Pages/Shared/Header.cshtml.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Durur.Modules.Generic.Entities.Model;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -9,8 +10,12 @@ namespace Durur.Erp.Api.Pages.Shared
 {
     public class HeaderModel : PageModel
     {
+        [BindProperty]
+        public User AuthUser { get; set; } = new User() { IsActive = false };
+
         public void OnGet()
         {
         }
+
     }
 }
