2a5df87696b01e420f2fc4d4470a76f39c5fe8d6
Ümit Durur <umit_durur@hotmail.com>
2021-12-29 15:28:23 +0300

    User register page validation added

---
+++ Durur.Erp.Api/wwwroot/js/custom/authentication/formValidation.js
@@ -0,0 +1,52 @@
+﻿var validator;
+$(document).ready(function () {
+    $(function () {
+
+        //$('select[required]').css({
+        //    position: 'absolute',
+        //    display: 'inline',
+        //    height: 0,
+        //    padding: 0,
+        //    border: '1px solid rgba(255,255,255,0)',
+        //    width: 0
+        //});
+
+        validator=$("#register-form").validate({
+            rules: {
+                username: {
+                    required: true,
+                    minlength: 5
+                },
+                email: {
+                    required: true,
+                    email: true
+                },
+                password: {
+                    required: true,
+                    minlength: 5
+                },
+                "password-again": {
+                    required: true,
+                    minlength: 5,
+                    equalTo: "#password"
+                },
+            },
+            //For custom messages
+            messages: {
+                username: {
+                    required: "Enter a username",
+                    minlength: "Enter at least 5 characters"
+                },
+            },
+            errorElement: 'div',
+            errorPlacement: function (error, element) {
+                var placement = $(element).data('error');
+                if (placement) {
+                    $(placement).append(error)
+                } else {
+                    error.insertAfter(element);
+                }
+            }
+        });
+    });
+});
---
+++ Durur.Erp.Api/wwwroot/js/custom/authentication/register-page.js
@@ -0,0 +1,45 @@
+﻿$(document).ready(function () {
+    var regbutton = document.getElementById("register-button");
+    regbutton.addEventListener("click", function (r) {
+        r.preventDefault();
+
+        $("#register-form").valid();
+        console.log(validator.numberOfInvalids());
+        if (validator.numberOfInvalids() === 0) {
+            //var userModel = {
+            //    userName: $("[name='username']").val(),
+            //    email: $("[name='email']").val(),
+            //        passwordHash: $("[name='password']").val()
+            //};
+            var userModel = {
+                userName: document.getElementById('username').value,
+                email: document.getElementById('email').value,
+                passwordHash: document.getElementById('password').value,
+            };
+
+            $.ajax({
+                type: "POST",
+                url: "/api/login/create",
+                contentType: "application/json;charset=utf-8",
+                data: JSON.stringify(userModel),
+                dataType: "json",
+                success: function (data) {
+                    Swal.fire({
+                        text: "Succesfully registered.",
+                        icon: "success",
+                        buttonsStyling: !1,
+                        confirmButtonText: "Ok, got it!",
+                        customClass: { confirmButton: "btn btn-primary" }
+                    }).then((function (t) {
+                        if (t.isConfirmed === true)
+                            window.location.href = "Login";
+                    }));
+                },
+                error: function (data) {
+                    console.log(data);
+                }
+            });
+        }
+
+    });
+});
